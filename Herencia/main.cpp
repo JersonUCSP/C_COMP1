da907f317863550aeaf2eb7ce06c75c868bd3eeb
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2017-05-19 13:37:35 -0500

    main

---
+++ Herencia/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+#include "Bycicle.h"
+using namespace std;
+
+int main()
+{
+
+   Vehicle v1("837",2017);
+   Car a1("234",2005,"Automatic");
+   Truck e1("152",1997,85654451);
+   Bycicle i1("561",2011,"Amarillo");
+
+   cout<<Vehicle.v1;
+
+}
+ //verhiculo carro, biciletam, truck
